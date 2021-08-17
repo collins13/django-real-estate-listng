bef5732b0caf5f8a51e8088bef70ca5e12d30ca9
collins13 <rashidcollins16@gmail.com>
2021-08-17 12:10:55 +0300

    changes

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Listing;
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 
 
 def index(request):
-    return render(request, 'listings/listings.html');
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6);
+    page = request.GET.get("page");
+    page_listing = paginator.get_page(page)
+    contex = {
+        'listings':page_listing
+    }
+    return render(request, 'listings/listings.html',contex);
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html');
 
 def search(request):
